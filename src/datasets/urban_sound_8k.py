a9f7abec4e856f4e4f55932c5a86838bb45506ee
ilan <ilan.bacry20@gmail.com>
2026-02-17 11:50:36 +0100

    remove cache

--- src/datasets/urban_sound_8k.py
+++ src/datasets/urban_sound_8k.py
@@ -3,7 +3,7 @@ import torch
 import torchaudio
 from torch.utils.data import Dataset
 import kagglehub
-import hashlib
+
 
 class UrbanDataset(Dataset):
     def __init__(self, root_dir, sample_rate=16000, max_len=16000):
@@ -12,9 +12,7 @@ class UrbanDataset(Dataset):
         self.sample_rate = sample_rate
         self.max_len = max_len
         self.data = []
-        self.cache_dir = os.path.join(self.base_path, "cache")
-        os.makedirs(self.cache_dir, exist_ok=True)
-
+        self.cache = {}
 
         self.classes = sorted([
             d for d in os.listdir(self.root_dir)
@@ -32,23 +30,19 @@ class UrbanDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
         path, label = self.data[idx]
-        cache_path = self._cache_path(path)
-
-        if os.path.exists(cache_path):
-            waveform = torch.load(cache_path)
-            return waveform, label
 
         waveform, sr = torchaudio.load(path)
-        waveform = waveform.mean(dim=0)
+        waveform = waveform.mean(dim=0) 
 
         if sr != self.sample_rate:
             waveform = torchaudio.functional.resample(
                 waveform, sr, self.sample_rate
             )
 
+        
         if waveform.shape[0] < self.max_len:
             waveform = torch.nn.functional.pad(
                 waveform, (0, self.max_len - waveform.shape[0])
@@ -58,5 +52,4 @@ class UrbanDataset(Dataset):
 
         waveform = (waveform - waveform.mean()) / (waveform.std() + 1e-9)
 
-        torch.save(waveform, cache_path)
         return waveform, label
